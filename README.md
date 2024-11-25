072aa8fcdfcc99373d3c2b20c4f146e970bef263
danielsychocki <145569727+danielsychocki@users.noreply.github.com>
2024-11-25 08:54:18 -0300

    Finalizado.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project_flashcard
+# Projeto-2-serie-3-trimestre
--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,33 @@
 criaCartao(
     'Programação',
     'O que é Python?',
-    'O Python é uma linguagem de pogramação'
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma Variavel?',
-    'Uma variavel é um elemento que permite o armazenamento de valores na memória do computador'
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de codigo que executa alguma tarefa'
+    'Mátematica',
+    'Quantos é 5 + 5?',
+    'É 10'
 )
 
 criaCartao(
-    'Programação',
-    'O que é Back-end?',
-    'Back-end é a parte do desenvolvimento web que lida com a lógica do servidor, banco de dados e a comunicação entre o cliente e o servidor'
+    'Mátematica',
+    'Quantos é 23 + 5?',
+    'É 28'
 )
-
 criaCartao(
-    'Tradução',
-    'Como se diz Bom dia na lingua Inglesa?',
-    'Se diz "Good morning" '
+    'Mátematica',
+    'Quantos é 1 + 11?',
+    'É 12'
 )
-
 criaCartao(
-    'Futebol',
-    'Qual é o melhor time de Futebol do Mundo?',
-    'O melhor time de Futebol do mundo é O PALMEIRAS'
+    'Mátematica',
+    'Quantos é 60 + 4?',
+    'É 64'
 )
